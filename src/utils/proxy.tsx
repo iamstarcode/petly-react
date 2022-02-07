d199bfd3634d2d8206b4b4ae1dc4e121dca3ae55
iamstarcode <66052346+iamstarcode@users.noreply.github.com>
2022-02-07 13:15:57 +0100

    Update proxy

--- src/utils/proxy.tsx
+++ src/utils/proxy.tsx
@@ -1,7 +1,7 @@
 
 export const getLinkForProxy = (link :string) =>{
 
-    const proxyUrl = 'https://image-proxy-2.herokuapp.com/'
+    const proxyUrl = 'https://petly-proxy.iamstarcode.com/'
     const pos = link.lastIndexOf('/')
     const subString = link.substring(pos+1, link.length)
     return proxyUrl+subString
