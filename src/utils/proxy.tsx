64d5fa0c956d9e7717d9db6f01b92d0fdf4bbecb
Bakare Abiola <murphybiola007@gmail.com>
2022-02-07 20:26:28 +0800

    *

--- src/utils/proxy.tsx
+++ src/utils/proxy.tsx
@@ -5,4 +5,5 @@ export const getLinkForProxy = (link :string) =>{
     const pos = link.lastIndexOf('/')
     const subString = link.substring(pos+1, link.length)
     return proxyUrl+subString
+    
 }
